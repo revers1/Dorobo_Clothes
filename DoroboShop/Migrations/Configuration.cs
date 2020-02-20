ea3f68d7a2333e1d8733362edff93848c978b912
Veremeychuk <Veremeychukden@gmail.com>
2020-02-20 20:35:44 +0200

    Add seeder

--- DoroboShop/Migrations/Configuration.cs
+++ DoroboShop/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace DoroboShop.Migrations
 {
+    using DoroboShop.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,10 +17,32 @@ namespace DoroboShop.Migrations
 
         protected override void Seed(DoroboShop.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.Roles.Any())
+            {
+                IdentityRole roleAdmin = new IdentityRole()
+                {
+                    Name = "Admin",
+                };
+                IdentityRole roleUser = new IdentityRole()
+                {
+                    Name = "User",
+                };
+                roleManager.Create(roleAdmin);
+                roleManager.Create(roleUser);
+            }
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+            if (!userManager.Users.Any())
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                };
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+                userManager.Create(user, "Qwerty-1");
+                userManager.AddToRole(user.Id, "Admin");
+            }
         }
     }
 }
